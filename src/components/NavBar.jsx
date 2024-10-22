95ee590ff31279421d44fbf36ad2e6c221fc1c83
jordan barani <jordan.barani@student.moringaschool.com>
2024-10-22 20:16:13 +0300

    orderspage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,7 @@
-<<<<<<< HEAD:src/components/NavBar.jsx
-import { Link } from "react-router-dom";
-=======
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
 import { CiMenuBurger } from "react-icons/ci";
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,7 +14,6 @@ const Navbar = () => {
   }, []);
 
   const Logout = () => {
-
     Swal.fire({
       title: 'Are you sure?',
       text: "You will be logged out of the application.",
@@ -39,19 +35,12 @@ const Navbar = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
->>>>>>> 6c3cf24e0d94c9c86237ff958612f1ccbd74aa07:src/components/NavBar.js
 
-const NavBar = () => {
   return (
     <nav
       style={{ backgroundColor: "#CBE3E5" }}
-<<<<<<< HEAD:src/components/NavBar.jsx
       className="flex justify-between items-center p-4 text-black rounded-bl-3xl rounded-br-3xl"
     >
-=======
-      className="flex justify-between items-center p-4 text-black rounded-bl-3xl rounded-br-3xl">
-      
->>>>>>> 6c3cf24e0d94c9c86237ff958612f1ccbd74aa07:src/components/NavBar.js
       <div className="flex items-center space-x-2">
         <Link to="/" className="flex items-center">
           <img
@@ -61,8 +50,6 @@ const NavBar = () => {
           />
         </Link>
       </div>
-
-
       <ul className="hidden md:flex space-x-4 items-center">
         <li>
           <Link to="/" className="hover:underline">
@@ -75,13 +62,11 @@ const NavBar = () => {
           </Link>
         </li>
         <li>
-        </li>
-          <li>
           <Link to="/vendor" className="hover:underline">
             Vendor Page
           </Link>
-          </li>
-          <li>
+        </li>
+        <li>
           <Link to="/cart" className="flex items-center hover:underline">
             <img
               src="https://i.pinimg.com/originals/66/22/ab/6622ab37c6db6ac166dfec760a2f2939.gif"
@@ -95,14 +80,15 @@ const NavBar = () => {
             Checkout
           </Link>
         </li>
+        <li>
+          <Link to="/orders" className="hover:underline">
+            Orders
+          </Link>
+        </li>
       </ul>
-
       <div className="md:hidden">
-        
-      <CiMenuBurger className="size-6" onClick={toggleMenu} />  
-
+        <CiMenuBurger className="size-6" onClick={toggleMenu} />
       </div>
-
       {isOpen && (
         <ul className="absolute top-20 left-0 w-full bg-gray-100 flex flex-col space-y-4 items-center p-4 md:hidden">
           <li>
@@ -120,17 +106,24 @@ const NavBar = () => {
               {isOpen ? (
                 <span className="text-lg">Cart</span>
               ) : (
-              <img
-                src="https://i.pinimg.com/originals/66/22/ab/6622ab37c6db6ac166dfec760a2f2939.gif"
-                alt="Cart"
-                className="h-10 w-10"
-              />
+                <img
+                  src="https://i.pinimg.com/originals/66/22/ab/6622ab37c6db6ac166dfec760a2f2939.gif"
+                  alt="Cart"
+                  className="h-10 w-10"
+                />
               )}
             </Link>
           </li>
-          <button onClick={Logout}>
-            logout
-          </button>
+          <li>
+            <Link to="/orders" className="hover:underline" onClick={() => setIsOpen(false)}>
+              Orders
+            </Link>
+          </li>
+          <li>
+            <button onClick={Logout} className="hover:underline">
+              Logout
+            </button>
+          </li>
         </ul>
       )}
     </nav>
